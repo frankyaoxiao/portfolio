dd23b57cf41ea892be93a883769f09113469f0ee
frankyaoxiao <frankyaoxiao@gmail.com>
2020-04-17 22:48:11 +0000

    updated

---
+++ index.js
@@ -0,0 +1,9 @@
+//Bad langauge(Netscapes fault), but all the good animations use it
+//I would use line tracing, but you need a svg and I'm too lazy
+
+ScrollReveal({ reset: true });
+ScrollReveal({  delay: 500 });
+ScrollReveal({  distance: '75px' });
+ScrollReveal({  origin: 'left' });
+ScrollReveal().reveal('.test')
+
